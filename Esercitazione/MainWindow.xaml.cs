02d3b69fff381bcc0df1504c52e9468e08f93de7
narcisbuzatu2003@gmail.com <narcisbuzatu2003@gmail.com>
2020-01-04 19:54:21 +0100

    Fine dopo la creazione dei file con numeri,nomi,validi,invalidi. Trovati i voti validi e scritti sulla listbox. Ci sono dei problemi da rivedere(imparare a cancellare una riga del file se non segue i parametri di un if).

--- Esercitazione/MainWindow.xaml.cs
+++ Esercitazione/MainWindow.xaml.cs
@@ -24,11 +24,12 @@ namespace Esercitazione
         string filei = @"invalidi.txt";
         string filev = @"validi.txt";
         string voti = @"voti.txt";
-        string prova = @"prova.txt";
+        string nomi = @"prova.txt";
         public MainWindow()
         {
             InitializeComponent();
-            if(File.Exists(filei))
+            if (File.Exists(filei))
+                //divisione voti
                 using (StreamReader readerinvalido = new StreamReader(filei))
                 {
                     using (StreamWriter writernumeri = new StreamWriter(voti))
@@ -37,34 +38,101 @@ namespace Esercitazione
                         string linei = readerinvalido.ReadLine();
                         while ((linei = readerinvalido.ReadLine()) != null)
                         {
-                            int found = 0;                                                        
-                                Console.WriteLine(linei);
+                            int found = 0;
+                            Console.WriteLine(linei);
                             {
                                 found = linei.IndexOf(",");
-                                writernumeri.WriteLine("{0}", linei.Substring(found+1));
+                                writernumeri.WriteLine("{0}", linei.Substring(found + 1));
                             }
                         }
                     }
-
-
-                    using (StreamReader readernumeri = new StreamReader(voti))
+                }
+            //voto max
+            using (StreamReader readernumeri = new StreamReader(voti))
+            {
+                string linen = readernumeri.ReadLine();
+                double max = 0;
+                while ((linen = readernumeri.ReadLine()) != null)
+                {
+                    double n = double.Parse(linen);
+                    if (n > 0 && n <= 10 && n > max)
                     {
-                        string linen = readernumeri.ReadLine();
-                        double max = 0;
-                        while ((linen = readernumeri.ReadLine()) != null)
-                        {                            
-                            double n = double.Parse(linen);                            
-                            if (n>max)
-                            {
-                                max = n;
-                            }                            
-                        }
-                        lbl_max.Content = $"Il voto più alto è stato {max}";
+                        max = n;
                     }
+                }
+                lbl_max.Content = $"Il voto più alto è stato {max}";
+            }
 
 
-                    
-                }                            
-        }   
+            //divisione nomi
+            using (StreamWriter writervalidi = new StreamWriter(nomi))
+            {
+                using (StreamReader readerin = new StreamReader(filei))
+                {
+                    writervalidi.WriteLine("NOMI");
+                    string linev = readerin.ReadLine();
+                    while ((linev = readerin.ReadLine()) != null)
+                    {
+                        Console.WriteLine(linev);
+                        {
+                            int pos = linev.IndexOf(",");
+                            if (pos >= 0)
+                            {
+                                string afterFounder = linev.Remove(pos);
+                                writervalidi.WriteLine(afterFounder);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            //lettura solo dei validi
+            using (StreamReader readervoti = new StreamReader(voti))
+            {
+                string vline = readervoti.ReadLine();
+
+                using (StreamReader readernomi = new StreamReader(nomi))
+                {
+                    string nline = readernomi.ReadLine();
+
+                    using (StreamWriter writervalidi = new StreamWriter(filev))
+                    {
+                        writervalidi.WriteLine("VALIDI");
+                        while ((vline = readervoti.ReadLine()) != null)
+                        {
+                            while ((nline = readernomi.ReadLine()) != null)
+                            {
+                                string nome = nline;
+                                string virgola = ",";
+                                double n;
+                                n = double.Parse(vline);
+                                if (n < 0 || n > 10)
+                                {
+                                    writervalidi.WriteLine("");
+                                }
+                                else
+                                {
+                                    writervalidi.WriteLine($"{nome}{virgola}{n}");
+                                }
+                                break;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+
+            //scrittura nella listbox
+            using (StreamReader readervalidi = new StreamReader(filev))
+            {                
+                string line = readervalidi.ReadLine();
+                while ((line = readervalidi.ReadLine()) != null)
+                {
+                    lbx_cronologia.Items.Add(line.ToString());
+                }
+            }
+        }
     }
 }
